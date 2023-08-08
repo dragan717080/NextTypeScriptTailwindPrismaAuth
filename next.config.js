9448aee7b677d1349a2718b68b2e904e881d88b3
= <hardlane2000@gmail.com>
2023-08-08 12:20:55 +0200

    adding shadcn @radix-ui support in next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config, options) => {
+      config.module.rules.push({
+          test: /\.mjs/,
+          include: /node_modules/,
+          type: 'javascript/auto',
+      });
+      return config;
+  },
+  experimental: {
+      serverActions: true
+  },
+  images: {
+      domains: [],
+      formats: ['image/avif', 'image/webp']
+  }
+}
 
-module.exports = nextConfig
+module.exports = nextConfig;
